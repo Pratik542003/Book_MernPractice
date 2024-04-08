4b158ae3d7ff2f2daca5d1e28464bdd5081cb1b6
Pratik <dhanepratik543@gmail.com>
2024-04-08 22:15:26 +0530

    MERN Practice

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+const express =require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Book  = require('./models/bookModel');
+
+const bookRoute  = require('./routes/bookroutes');
+const cors = require('cors');
+
+port=3000;
+
+app.use(express.json());
+
+app.use(cors());
+app.use(
+  cors({
+    origin:'127.0.0.1:3000',
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders:['Content-Type'],
+  })
+)
+
+
+const mongoDBURL ='mongodb+srv://dhanepratik543:S14J33F9esYs1Ib2@book.ridhpsu.mongodb.net/';
+
+app.get('/',(req,res)=>{
+    console.log(req);
+    return res.status(234).send("Welcome");
+})
+
+app.use('/',bookRoute)
+
+
+//Mongoose connection
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(port, () => {
+      console.log(`App is listening to port: ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
